6443b571be268ff6157d297f377085223649f0f7
Sergij <Sergij@172.31.0.17>
2018-02-08 20:33:10 +0200

    Second version CartDAO

--- Eshop/src/org/itstep/dao/CartDAO.java
+++ Eshop/src/org/itstep/dao/CartDAO.java
@@ -9,7 +9,13 @@ public class CartDAO {
 	public void saveAction(int orderID, Account account) {
 		Connection connection = null;
 		PreparedStatement statement = null;
-		String sql = "INSERT INTO carts(acclogin, timeCreation) VALUES(?, ?, ?, ?)";
+		String sql = "INSERT INTO carts(acclogin, orderID, timeCreation) VALUES(?, ?, ?)";
+		
+		connection = ConnectionToDB.getConnection();
+		statement = connection.prepareStatement(sql);
+		statement.setString(1, acclogin);
+		statement.setInt(2, orderID);			
+		statement.setLong(3, 999999);
 
 }
 }
