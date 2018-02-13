7aec371daadbfe5c8731b37918e5664b12002363
rex <rex@rex>
2018-02-13 19:48:33 +0200

    DBConnection change

--- Eshop/src/org/itstep/dao/DBConnection.java
+++ Eshop/src/org/itstep/dao/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL = "jdbc:postgresql://localhost:5432/home_task";
+	private static final String URL = "jdbc:postgresql://localhost:5432/EShop";
 	private static final String USER_NAME = "postgres";
 	private static final String USER_PASSWORD = "qqq";
 
